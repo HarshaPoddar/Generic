f8d9f46f521f3ddca47964c906715aa35829b5fd
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-01-27 13:01:13 +0530

    Complete Generic Class

---
+++ Execute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericAssignment
+{
+    class Mycollection<T>
+    {
+        public T[] list = new T[10];
+        public T this[int i]
+        {
+            get
+            {
+                if (i < 0 && i > -10)
+                    return list[9];
+                else
+                    return list[i];
+                
+            }
+            set
+            {
+                if (i >= 0 && i <=10)
+                {
+                    list[i] = value;
+                }
+                else if (i < 0 && i > -10)
+                {
+                    list[9] = value;
+                }
+                else
+                    Console.WriteLine("Out of Bounds!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Mycollection<int> mycollection = new Mycollection<int>();
+            int[] items = new int[10]; 
+       
+            Console.WriteLine("Enter the 10 values: ");
+            for (int counter = 0; counter < 10; counter++)
+            {
+
+                items[counter] =Convert.ToInt16(Console.ReadLine());
+            }
+            
+
+            Console.WriteLine("Enter the index of the item to be displayed");
+            int index = Convert.ToInt16(Console.ReadLine());
+            
+            if (index < 0 && index >-10)
+            mycollection[index] = items[10+index];
+            else
+                mycollection[index] = items[index];
+                
+            Console.WriteLine("Your value at index {0} = " , index);
+            Console.WriteLine(mycollection[index]);
+            Console.ReadKey();
+        }
+    }
+}

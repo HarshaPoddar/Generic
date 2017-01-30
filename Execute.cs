02f56094f73e579d292570d59817d1ae9856a029
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-01-30 23:14:23 +0530

    Just a minor change.

--- Execute.cs
+++ Execute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace genericAssignment
+namespace GenericAssignment
 {
     class Mycollection<T>
     {
--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace genericAssignment
+namespace GenericAssignment
 {
-    class Program
+    class Generic
     {
         public static void Main(string[] args)
         {
